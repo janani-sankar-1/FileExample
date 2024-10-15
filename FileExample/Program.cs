432d98baff09f4016171b65b548fd8e1b80a7010
Janani S <Janani S@DESKTOP-13MT9AS>
2024-10-15 11:43:44 +0530

    Add project files.

---
+++ FileExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FileExample
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            //File.Create(@"c:\practice\India.txt").Close();
+            //@ disables escape sequence character "\"
+
+            string filePath = @"c:\practice\India.txt", filePath2 = @"c:\practice\India2.txt", filePath3=@"c:\practice\another.txt";
+            File.Create(filePath).Close();
+            Console.WriteLine("India.txt created");
+
+            bool exists=File.Exists(filePath);
+            if (exists)
+            {
+                File.Copy(filePath, filePath2);
+                Console.WriteLine("Copied India.txt to India2.txt");
+
+                File.Move(filePath2, filePath3);
+                Console.WriteLine("copied successfully");
+
+                File.Delete(filePath3);
+                Console.WriteLine("another.txt deleted");
+            }
+            else
+            {
+                Console.WriteLine("File not found");
+            }
+            Console.ReadKey();
+        }
+    }
+}
